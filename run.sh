f8c1670b19e676afa88c88ee3d58acf0e8003015
Marcio Goda <marciogoda@gmail.com>
2020-02-17 12:27:24 +0000

    fixed run.sh

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 
 docker build -t get-secret .
 
-docker run -it --rm -e AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY -e AWS_SESSION_TOKEN -e AWS_DEFAULT_REGION -v ~/:/home --name get-secret get-secret python build_npmrc.py platform/$1/jenkins_npm_repository_config
+docker run -it --rm -e AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY -e AWS_SESSION_TOKEN -e AWS_DEFAULT_REGION -v ~/:/home --name get-secret get-secret python build_npmrc.py $1
